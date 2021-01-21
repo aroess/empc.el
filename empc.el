22c676d1d4f598aa4b70b465d2cbd567582e2aed
aroess <aroess@posteo.de>
2021-01-21 18:30:31 +0000

    Update empc.el

--- empc.el
+++ empc.el
@@ -1,6 +1,3 @@
-;; This file is part of the
-;; Copyright (c) 2015 Liviu Ionescu.
-
 ;; This program is free software: you can redistribute it and/or modify  
 ;; it under the terms of the GNU General Public License as published by  
 ;; the Free Software Foundation, version 3.
