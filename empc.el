80d7a1da9a9901c25c4388f428a9e4e2c7f7fb5b
aroess <aroess@users.noreply.github.com>
2022-03-17 21:33:22 +0100

    Update empc.el

--- empc.el
+++ empc.el
@@ -1,6 +1,7 @@
 ;;; empc.el --- A simple elisp wrapper for mpd and mpc.
 
 ;;; Commentary:
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, version 3.
